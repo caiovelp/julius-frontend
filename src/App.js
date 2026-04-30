011a15f393bdd0f9c4c00fdd08f3fcf6e44b725e
Caio Paranhos <caio.paranhos@netlex.com.br>
2026-04-30 14:19:51 -0300

    issue-9: Turn login optional and redirect directly to front page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import CadastroPage from "./pages/CadastroPage";
 import LoginPage from "./pages/LoginPage";
 import CarteiraPage from "./pages/CarteiraPage";
 import ConfiguracaoPage from "./pages/ConfiguracaoPage";
 
+// Default guest user ID when authentication is disabled
+const GUEST_USER_ID = process.env.REACT_APP_GUEST_USER_ID || '1';
+
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Root route redirects to guest wallet */}
+        <Route path="/" element={<Navigate to={`/carteira/${GUEST_USER_ID}`} replace />} />
+        
         <Route path="/register" element={<CadastroPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/carteira/:id" element={<CarteiraPage />} />
